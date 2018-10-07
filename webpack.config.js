582b6f7a2415a2ed5f3f0eb6ab5ebc4e75fe315f
Bartek <14033600+artbarte@users.noreply.github.com>
2018-10-07 22:09:19 +0200

    add config files

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: "./src/game.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [new CleanWebpackPlugin(["dist"])],
+    devServer: {
+        contentBase: "./",
+        publicPath: "/dist/",
+    },
+    mode: "none",
+    devtool: "inline-source-map",
+};
