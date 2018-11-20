e1efed58014325ae36c152ca9d065085ae525d67
Bartek <14033600+artbarte@users.noreply.github.com>
2018-11-20 18:37:56 +0100

    add board wall collisions and fixes

--- src/game.js
+++ src/game.js
@@ -15,39 +15,46 @@ const app = new Application({
 
 document.body.appendChild(app.view);
 
-// loader.add("img/tetris.png").load(setup);
-
 const tileSize = 20;
-const snake = new Snake(tileSize,app.view.height,app.view.width);
-const food = new Food(tileSize,app.view.height,app.view.width);
+let snake;
+let food;
 
 function setup() {
+    snake = new Snake(tileSize, app.view.height, app.view.width);
+    app.stage.addChild(snake);
+
+    food = new Food(tileSize, app.view.height, app.view.width);
+    app.stage.addChild(food);
+
+    // Set Snake starting point
+    snake.x = app.view.width / 2;
+    snake.y = app.view.height / 2 - tileSize;
+
+
+    // Handle keyboard events
     window.addEventListener('keydown', (e) => {
         switch (e.key.toUpperCase()) {
-        case 'W': snake.vx = 0; snake.vy = -1; break;
-        case 'A': snake.vx = -1; snake.vy = 0; break;
-        case 'S': snake.vx = 0; snake.vy = 1; break;
-        case 'D': snake.vx = 1; snake.vy = 0; break;
-        case ' ': snake.grow(); break;
+        case 'W': snake.currDirection = snake.directions.NORTH; break;
+        case 'A': snake.currDirection = snake.directions.WEST; break;
+        case 'S': snake.currDirection = snake.directions.SOUTH; break;
+        case 'D': snake.currDirection = snake.directions.EAST; break;
         default: break;
         }
     });
 
-    app.stage.addChild(snake);
-    snake.x = app.view.width / 2;
-    snake.y = app.view.height / 2 - tileSize;
-
-    app.stage.addChild(food);
     gameLoop();
 }
 
-function gameLoop(delta) {
+function gameLoop() {
     snake.move();
-    if (snake.body[0].getGlobalPosition().x == food.position.x && snake.body[0].getGlobalPosition().y == food.position.y) {
-        
-        snake.grow()
-        food.generateNew()
+
+    // Think about changing dealing with snake eating food in Snake class
+    if (snake.body[0].getGlobalPosition().x === food.position.x
+    && snake.body[0].getGlobalPosition().y === food.position.y) {
+        snake.grow();
+        food.generateNew();
     }
+    // Set gameloop at 15 frames per second
     setTimeout(gameLoop, 1000 / 15);
 }
 setup();
--- src/snake.js
+++ src/snake.js
@@ -1,3 +1,8 @@
+function mod(a, n) {
+    return a - (n * Math.floor(a / n));
+}
+
+
 class Rectangle extends PIXI.Graphics {
     constructor(w, h, color = 0xf82929) {
         super();
@@ -10,70 +15,72 @@ class Rectangle extends PIXI.Graphics {
     }
 }
 export class Food extends Rectangle {
-    constructor(size,boardH, boardW) {
-        super(size,size);
-        this.size = size
-        this.boardW = boardW
-        this.boardH = boardH
-        this.generateNew()
+    constructor(size, boardH, boardW) {
+        super(size, size, 0x4286f4);
+        this.size = size;
+        this.boardW = boardW;
+        this.boardH = boardH;
+        this.generateNew();
     }
+
     generateNew() {
-        this.x = (Math.floor(Math.random() * this.boardW / this.size)) * this.size
-        this.y = (Math.floor(Math.random() * this.boardH / this.size)) * this.size
+        this.x = (Math.floor(Math.random() * this.boardW / this.size)) * this.size;
+        this.y = (Math.floor(Math.random() * this.boardH / this.size)) * this.size;
     }
 }
 export class Snake extends PIXI.Container {
-    constructor(size,boardH,boardW) {
+    constructor(size, boardH, boardW) {
         super();
         this.size = size;
-        this.boardW = boardW
-        this.boardH = boardH
+        this.boardW = boardW;
+        this.boardH = boardH;
         this.vx = 0;
         this.vy = 0;
         this.body = this.children;
         this.head = new PIXI.Point(0, 0);
 
+        this.directions = {
+            NORTH: [0, -1],
+            EAST: [1, 0],
+            SOUTH: [0, 1],
+            WEST: [-1, 0],
+            STOP: [0, 0],
+        };
+        this.currDirection = this.directions.STOP;
         this.grow();
     }
 
     grow() {
         const cell = new Rectangle(this.size, this.size);
         const added = this.addChild(cell);
-        
+
         if (this.body.length > 1) {
-            added.position.x = this.body[this.body.length -2].x
-            added.position.y = this.body[this.body.length -2].y
-            console.log(added.position)
+            added.position.x = this.body[this.body.length - 2].x;
+            added.position.y = this.body[this.body.length - 2].y;
         }
-        
-        
-        
     }
 
     move() {
-    //     this.head.x += (this.vx * this.size % this.boardW)* this.size;
-    //     this.head.y += (this.vy * this.size % this.boardH)* this.size;
-        this.head.x += this.vx * this.size;
-        this.head.y += this.vy * this.size;
-        
-        for (let i = 0;i < this.body.length - 1;i++) {
-            console.log(this.head.x == this.body[i].x && this.head.y == this.body[i].y)
-            if (i === 0) continue
-            if (this.head.x == this.body[i].x && this.head.y == this.body[i].y){
-                
-                
-                this.removeChildren(1)
-                this.head.x = 0
-                this.head.y = 0
+        // Calculate next head position
+        this.head.x += this.currDirection[0] * this.size;
+        this.head.y += this.currDirection[1] * this.size;
+
+        // Complicated way using modulo (because JS % buggy(???) to dectect with snake went out of board)
+        this.head.x = mod((this.body[0].getGlobalPosition().x), this.boardW) - this.body[0].getGlobalPosition().x + this.head.x;
+        this.head.y = mod((this.body[0].getGlobalPosition().y), this.boardH) - this.body[0].getGlobalPosition().y + this.head.y;
+
+        // Check if next head is not colliding with any other parts
+        for (let i = 0; i < this.body.length - 1; i++) {
+            if (i === 0) continue;
+            if (this.head.x == this.body[i].x && this.head.y == this.body[i].y) {
+                this.removeChildren(1);
+                this.head.x = 0;
+                this.head.y = 0;
             }
         }
 
-        for (let i = 0; i > this.body.length - 1; i++) {
-            this.setChildIndex(this.getChildAt(i), i + 1);
-        }
-
-        const lastElem = this.body[this.body.length - 1];
-        lastElem.position = this.head;
-        this.addChildAt(lastElem, 0);
+        const lastBodyPart = this.body[this.body.length - 1];
+        lastBodyPart.position = this.head;
+        this.addChildAt(lastBodyPart, 0);
     }
 }
